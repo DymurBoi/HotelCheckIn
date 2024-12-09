9ad93d903629a7298f0ac818dd46582e373c81c6
DymurBoi <dymurboi@gmail.com>
2024-12-09 18:45:13 +0800

    user profile changes yes

--- updateprofile/forms.py
+++ updateprofile/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import UserProfile
 from accounts.models import CustomUser
+
 class ProfilePicForm(forms.ModelForm):
     profile_pic=forms.ImageField(label="Profile Picture")
     
@@ -28,5 +29,5 @@ class UserForm(forms.ModelForm):
             'username': forms.EmailInput(attrs={'class': 'form-input', 'placeholder': 'Enter your email'}),
             'address': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Enter your address'}),
             'phone_number': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Enter your phone number'}),
-            'profile_pic': forms.ClearableFileInput(attrs={'class': 'form-file-input'}),
+            'profile_pic': forms.FileInput(attrs={'class': 'form-file-input'}),
         }
